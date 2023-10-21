e334462eabba32407ac607fdef575edff32d357a
Artem Shtyrkhun <shtyrkhun2012@gmail.com>
2023-10-21 22:51:25 +0300

    psvm1

--- src/psvm.java
+++ src/psvm.java
@@ -1,6 +1,12 @@
 public class psvm {
     public static void main(String[] args) {
-        System.out.println("ease");
+       int days = 10000;
+       int year = days / 365;
+       int months = (days - (year*365))/30;
+       int leftdays = (days - (year * 365) - (months*30));
+        System.out.println(year);
+        System.out.println(months);
+        System.out.println(leftdays);
     }
 
 
